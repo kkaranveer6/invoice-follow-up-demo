ee098133bfc8fb40d2b4adceee4d464510121c5f
Karanveer <karanveerkar@gmail.com>
2026-03-29 20:38:03 +1030

    chore: delete all backend code, DB schema, and env files

--- prisma/migrations/20260301000000_add_subscription_fields/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "stripeCustomerId" TEXT,
-ADD COLUMN     "subscriptionStatus" TEXT NOT NULL DEFAULT 'inactive';
-
--- CreateIndex
-CREATE UNIQUE INDEX "User_stripeCustomerId_key" ON "User"("stripeCustomerId");
